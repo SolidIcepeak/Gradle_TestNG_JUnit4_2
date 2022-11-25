8c214f55fd78f00cee0e1059fc09b86e5160ea80
Solidicepeak <morozov.solid@gmail.com>
2022-11-25 16:45:00 +0500

    Добавил тесты на JUnit 4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void MainTest() {
+
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void ZeroTest() {
+
+        int amount = 0;
+        int expected = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void MaxTest() {
+
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+
+    }
+}
